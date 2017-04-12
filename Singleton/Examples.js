4485df873104d14a1fe5d81faf0cb06e0a05e5cf
Catherine Thompson <cathythompson0915@gmail.com>
2017-04-12 11:37:53 -0600

    add singleton

---
+++ Singleton/Examples.js
@@ -0,0 +1,41 @@
+var printer = (function () {
+
+  var printerInstance;
+
+  function create () {
+
+    function print() {
+      // underlying printer mechanics
+    }
+
+    function turnOn() {
+      // warm up
+      // check for paper
+    }
+
+    return {
+      // public + private states and behaviors
+      print: print,
+      turnOn: turnOn
+    };
+  }
+
+  return {
+    getInstance: function() {
+      if(!printerInstance) {
+        printerInstance = create();
+      }
+      return printerInstance;
+    }
+  };
+
+  function Singleton () {
+    if(!printerInstance) {
+      printerInstance = intialize();
+    }
+  };
+
+})();
+
+// can get the printer instance like so
+var officePrinter = printer.getInstance();
